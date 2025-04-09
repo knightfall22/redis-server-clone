fd8f92c86946757c05524ba88d4ff6099821eefe
knightfall22 <pelumi066@gmail.com>
2025-04-09 14:29:56 +0100

    Replication

--- app/main.go
+++ app/main.go
@@ -7,7 +7,13 @@ import (
 	"net"
 	"os"
 	"strings"
+	"sync"
 	"time"
+)
+
+var (
+	connections []io.Writer
+	connMu      sync.Mutex
 )
 
 func main() {
@@ -21,6 +27,8 @@ func main() {
 	replicaOf := flag.String("replicaof", "", "set has replica of a master")
 
 	flag.Parse()
+
+	fmt.Println("Port:", *port)
 
 	//Configuration setup
 	ConfigMu.Lock()
@@ -53,7 +61,8 @@ func main() {
 	if *replicaOf != "" {
 		conn, err := net.Dial("tcp", *replicaOf)
 		if err != nil {
-			fmt.Println("Failed to bind to port 6379")
+			fmt.Println("Failed to bind to port ", *replicaOf)
+			conn.Close()
 			os.Exit(1)
 		}
 		fmt.Println("Connected on to master")
@@ -64,17 +73,55 @@ func main() {
 		//Todo: There has to be a better way to do this
 		time.Sleep(time.Second * 1)
 		writer.Write(replconfLWriter(*port))
+
 		time.Sleep(time.Second * 1)
 		writer.Write(replconfCWriter())
+
 		time.Sleep(time.Second * 1)
 		writer.Write(psyncWrite())
+
+		go func(conn net.Conn) {
+			for {
+				resp := NewResp(conn)
+				value, err := resp.Read()
+
+				fmt.Println(value)
+				if err != nil {
+					fmt.Println("Error reading from connection", err.Error())
+					return
+				}
+
+				if value.typ != "array" {
+					fmt.Println("Invalid request, expected array")
+					continue
+				}
+
+				if len(value.array) == 0 {
+					fmt.Println("Invalid request, expected array length > 0")
+					continue
+				}
+
+				command := strings.ToUpper(value.array[0].bulk)
+				args := value.array[1:]
+
+				handle, ok := Handlers[command]
+				if !ok {
+					fmt.Println("Invalid command: ", command)
+					writer.Write(Value{typ: "string", str: ""})
+					continue
+				}
+
+				handle(args)
+			}
+		}(conn)
+
 	}
 
 	// Uncomment this block to pass the first stage
 	url := fmt.Sprintf("localhost:%s", *port)
 	l, err := net.Listen("tcp", url)
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
+		fmt.Println("Failed to bind to port ", *port)
 		os.Exit(1)
 	}
 
@@ -113,6 +160,16 @@ func main() {
 
 				writer := NewWriter(conn)
 
+				//Test propagation
+				if command == "SET" {
+					multi := io.MultiWriter(connections...)
+					_, err = multi.Write(value.Marshal())
+
+					if err != nil {
+						return
+					}
+				}
+
 				handle, ok := Handlers[command]
 				if !ok {
 					fmt.Println("Invalid command: ", command)
@@ -127,7 +184,12 @@ func main() {
 
 				if command == "PSYNC" {
 					writer.Write(fullsync())
+
+					connMu.Lock()
+					connections = append(connections, conn)
+					connMu.Unlock()
 				}
+
 			}
 		}(conn)
 	}
