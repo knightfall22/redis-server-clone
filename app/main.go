3a42cd2152a3557ae17e90e68ef8ae33cb967971
knightfall22 <pelumi066@gmail.com>
2025-04-10 15:02:12 +0100

    wait no commands

--- app/main.go
+++ app/main.go
@@ -113,12 +113,12 @@ func main() {
 					continue
 				}
 
-				val := handle(args)
+				handle(args)
 				writer := NewWriter(conn)
 
-				if command == "REPLCONF" && strings.ToUpper(args[0].bulk) == "GETACK" {
-					writer.Write(val)
-				}
+				// if command == "REPLCONF" && strings.ToUpper(args[0].bulk) == "GETACK" {
+				// 	writer.Write(val)
+				// }
 
 				valCopy := value
 				valcount := len(valCopy.Marshal())
